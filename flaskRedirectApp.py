4fee8d0ad38a24e57ac8eab5bc481bc3e9e824f6
mahmoud nayel <mahmmoudnayel2004@gmail.com>
2025-06-20 15:39:16 +0300

    newApp

---
+++ flaskRedirectApp.py
@@ -0,0 +1,11 @@
+from flask import Flask, redirect
+
+app = Flask(__name__)
+
+# simulate the expired path
+@app.route('/accounts/jinxboy/projects/knooz/builds/b0ee4444-420b-4094-9e9d-3a6cf36e60f7')
+def redirect_to_new_apk():
+    return redirect("https://www.youtube.com/results?search_query=how+to+use+expired+link+to+direct+to+a+link+i+want", code=302)
+
+if __name__ == '__main__':
+    app.run(debug=True)
